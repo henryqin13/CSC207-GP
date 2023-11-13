27a4a2e7dab52845e95971baf4e9730631c1638c
solairamu <solairamu18@gmail.com>
2023-11-13 10:07:44 -0500

    coded Player entity, now need to implement Role interface

---
+++ entity/Player.java
@@ -0,0 +1,41 @@
+package entity;
+
+public class Player {
+    private String name;
+    private int score;
+    private List<Role> roles;
+
+    public Player(String name, int score, List<Role> roles) {
+        this.name = name;
+        this.score = score;
+        this.roles = roles;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String toString() {
+        return "Player{ " + "name='" + name + '\'' + ", score=" + score + ", roles=" + roles + '}';
+    }
+}
