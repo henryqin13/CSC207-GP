be141fa62f06d5ecaf85076f88545286b5e52af4
jerryxia <jerryx3.141@gmail.com>
2023-11-13 10:41:51 -0500

    blueprint for guesscity use case

---
+++ use_case/GuessCityUseCase.java
@@ -0,0 +1,13 @@
+//package use_case;
+//
+//public class GuessCityUseCase {
+//    private CityRepository cityRepository;
+//
+//    public GuessCityUseCase(CityRepository cityRepository) {
+//        this.cityRepository = cityRepository;
+//    }
+//
+//    public String execute(Player player, String cityName, List<String> categories) {
+//        // logic for guessing the city
+//    }
+//}
