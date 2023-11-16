bf795541cde48b500a571dedd666822e89e5eb65
ronashkenazy <ronashkenazy04@gmail.com>
2023-11-16 12:03:58 -0500

    added fact interface and numfact class

---
+++ src/entity/NumFact.java
@@ -0,0 +1,12 @@
+package entity;
+
+public class NumFact implements Fact{
+    private String category;
+    private long value;
+
+    public void Fact(String category, long value){
+        this.category = category;
+        this.value = value;
+    }
+
+}
