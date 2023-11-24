407bbe3908c90473182fd32c37b6bd7d07811d51
zihanqin <henry.qin@mail.utoronto.ca>
2023-11-24 16:22:05 -0500

    minor adjustments to typo

--- src/interface_adapter/LoggedIn/LoggedInViewModel.java
+++ src/interface_adapter/LoggedIn/LoggedInViewModel.java
@@ -1,4 +1,4 @@
 package interface_adapter.LoggedIn;
 
-public class LoggedInState {
+public class LoggedInViewModel {
 }
--- src/interface_adapter/Signup/ViewModel.java
+++ src/interface_adapter/Signup/ViewModel.java
@@ -1,4 +1,4 @@
-package interface_adapter;
+package interface_adapter.Signup;
 
 import java.beans.PropertyChangeListener;
 
