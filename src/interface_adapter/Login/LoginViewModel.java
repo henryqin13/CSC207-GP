cd0e335567a362570331c9a31ed99271e7b0f154
ronashkenazy <ronashkenazy04@gmail.com>
2023-11-24 16:43:20 -0500

    Created LoginViewModel

--- src/interface_adapter/Login/LoginViewModel.java
+++ src/interface_adapter/Login/LoginViewModel.java
@@ -1,4 +1,33 @@
 package interface_adapter.Login;
 
-public class LoginViewModel {
+import interface_adapter.Signup.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class LoginViewModel extends ViewModel {
+    public final String TITLE = "Log in";
+    public final String USERNAME_FIELD = "Enter Username";
+    public final String PASSWORD_FIELD = "Enter Password";
+
+    public static final String LOGIN_BUTTON_NAME = "Log in";
+    public static final String CANCEL_BUTTON_NAME = "Cancel";
+
+    private LoginState state = new LoginState();
+    public LoginViewModel(){
+        super("Log in");
+    }
+    public void setState(LoginState state) {
+        this.state = state;
+    }
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    public void firePropertyChanged(){
+        support.firePropertyChange("state", null, this.state);
+    }
+    public void addPropertyChangeListener(PropertyChangeListener listener){
+        support.addPropertyChangeListener(listener);
+    }
+    public LoginState getState(){
+        return state;
+    }
 }
