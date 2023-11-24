be034667561d4e6e76e592ee46e9073742e101ea
zihanqin <henry.qin@mail.utoronto.ca>
2023-11-24 17:32:00 -0500

    setting up interface adapter

--- src/interface_adapter/Login/LoginPresenter.java
+++ src/interface_adapter/Login/LoginPresenter.java
@@ -1,4 +1,45 @@
 package interface_adapter.Login;
 
-public class LoginPresenter {
+import interface_adapter.LoggedIn.LoggedInState;
+import interface_adapter.LoggedIn.LoggedInViewModel;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.Signup.SignupState;
+import use_case.Login.LoginOutputBoundary;
+import use_case.Login.LoginOutputData;
+import use_case.Signup.SignupOutputBoundary;
+import use_case.Signup.SignupOutputData;
+
+public class LoginPresenter implements LoginOutputBoundary {
+
+    private final LoginViewModel loginViewModel;
+    private final LoggedInViewModel loggedInViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public LoginPresenter(ViewManagerModel viewManagerModel,
+                          LoggedInViewModel loggedInViewModel,
+                          LoginViewModel loginViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.loggedInViewModel = loggedInViewModel;
+        this.loginViewModel = loginViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(LoginOutputData response) {
+        // On success, switch to the logged in view.
+
+        LoggedInState loggedInState = loggedInViewModel.getState();
+        loggedInState.setUsername(response.getUsername());
+        this.loggedInViewModel.setState(loggedInState);
+        this.loggedInViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setActiveView(loggedInViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        LoginState loginState = loginViewModel.getState();
+        loginState.setUsernameError(error);
+        loginViewModel.firePropertyChanged();
+    }
 }
