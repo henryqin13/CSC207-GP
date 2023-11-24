8b2d344138695d9ca9f0a65d8b9fce42e1e32aa2
zihanqin <henry.qin@mail.utoronto.ca>
2023-11-24 17:21:31 -0500

    setting up log in use case

--- src/use_case/Login/LoginUserDataAccessInterface.java
+++ src/use_case/Login/LoginUserDataAccessInterface.java
@@ -1,4 +1,11 @@
 package use_case.Login;
 
+import entity.Player;
+
 public interface LoginUserDataAccessInterface {
+    boolean existsByName(String identifier);
+
+    void save(Player player);
+
+    Player get(String username);
 }
