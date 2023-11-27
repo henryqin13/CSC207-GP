5ef634ee67aff3dd625c0ec5eb07ca05aab851bb
zihanqin <henry.qin@mail.utoronto.ca>
2023-11-27 09:29:46 -0500

    changed minorly

--- src/use_case/Signup/SignupInputBoundary.java
+++ src/use_case/Signup/SignupInputBoundary.java
@@ -1,4 +1,6 @@
 package use_case.Signup;
 
 public interface SignupInputBoundary {
+    void execute(SignupInputData signupInputData);
 }
+
--- src/use_case/Signup/SignupUserDataAccessInterface.java
+++ src/use_case/Signup/SignupUserDataAccessInterface.java
@@ -1,4 +1,9 @@
 package use_case.Signup;
 
+import entity.Player;
+
 public interface SignupUserDataAccessInterface {
+    boolean existsByName(String identifier);
+
+    void save(Player player);
 }
