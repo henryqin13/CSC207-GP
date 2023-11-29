4c9664b6e7975be65c28d08acb02257ac66df3d3
jerryxia <jerryx3.141@gmail.com>
2023-11-28 19:37:21 -0500

    example implementation for gameover use case

--- src/use_case/GameOverUseCase.java
+++ src/use_case/GameOverUseCase.java
@@ -15,3 +15,23 @@ public class GameOver {
         this.player = player;
         this.presenter = presenter;
     }
+    public void execute() {
+        // check if the player's last answer is correct
+        PlayerAnswer lastAnswer = match.getUserAnswers().get(match.getUserAnswers().size() - 1);
+        if (lastAnswer.getAnswer().equalsIgnoreCase(match.getCorrectAnswer().getName())) {
+            // if correct, update the player's score and show a congratulatory message
+            player.setScore(player.getScore() + 10); // added arbitrary # points for a correct answer (group can decide later)
+            presenter.displayMessage("You got it right! The city is " + match.getCorrectAnswer().getName() + ".");
+        } else {
+            // if incorrect, show a sorry message and reveal the correct answer
+            presenter.displayMessage("Sorry, that's not correct. The city is " + match.getCorrectAnswer().getName() + ".");
+        }
+        // show the player's final score and some feedback
+        presenter.displayMessage("Your final score is " + player.getScore() + ".");
+        presenter.displayFeedback(player.getScore());
+        // ask the player if they want to play again or quit
+        presenter.displayOptions();
+    }
+}
+
+//a presenter needs to be implemented
