c596530cf8ab3f13584ee7171fddd2d93dda09cd
jerryxia <jerryx3.141@gmail.com>
2023-11-28 19:43:12 -0500

    implemented consolepresenter to try for gameover use case

---
+++ src/interface_adapter/ConsolePresenter.java
@@ -0,0 +1,27 @@
+package interface_adapter;
+
+public class ConsolePresenter implements Presenter {
+    // implement the methods for the presenter
+    public void displayMessage(String message) {
+        // print the message to the console
+        System.out.println(message);
+    }
+
+    public void displayOptions() {
+        // print the options to the console
+        System.out.println("Do you want to play again or quit?");
+        System.out.println("1. Play again");
+        System.out.println("2. Quit");
+    }
+
+    public void displayFeedback(int score) {
+        // print some feedback based on the score
+        if (score >= 80) {
+            System.out.println("You are awesome!");
+        } else if (score >= 50) {
+            System.out.println("You are good!");
+        } else {
+            System.out.println("You can do better!");
+        }
+    }
+}
