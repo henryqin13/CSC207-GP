282fdcee4ab7c807e5a0350108d233d40abd9b56
zihanqin <henry.qin@mail.utoronto.ca>
2023-11-30 22:17:12 -0500

    fixed the signup button (now after user signed up they will be successfully redirected to the log in screen

--- src/app/TemporaryMain.java
+++ src/app/TemporaryMain.java
@@ -29,18 +29,12 @@ public class TemporaryMain {
 
         CardLayout cardLayout = new CardLayout();
 
-        // The various View objects. Only one view is visible at a time.
         JPanel views = new JPanel(cardLayout);
         application.add(views);
 
-        // This keeps track of and manages which view is currently showing.
         ViewManagerModel viewManagerModel = new ViewManagerModel();
         new ViewManager(views, cardLayout, viewManagerModel);
 
-        // The data for the views, such as username and password, are in the ViewModels.
-        // This information will be changed by a presenter object that is reporting the
-        // results from the use case. The ViewModels are observable, and will
-        // be observed by the Views.
         LoginViewModel loginViewModel = new LoginViewModel();
         LoggedInViewModel loggedInViewModel = new LoggedInViewModel();
         SignupViewModel signupViewModel = new SignupViewModel();
