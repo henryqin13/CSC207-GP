83e432287100520821486c8c16288510965bea28
jerryxia <jerryx3.141@gmail.com>
2023-12-03 05:04:22 -0500

    set up the mock dependencies to begin testing guest use cases

---
+++ test/use_caseInteractor/GuestInteractorTest.java
@@ -0,0 +1,27 @@
+package use_caseInteractor;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import use_case.Guest.*;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class GuestInteractorTest {
+
+    private GuestUserDataAccessInterface userDataAccessObject;
+    private GuestOutputBoundary guestOutputBoundary;
+    private GuestInteractor guestInteractor;
+
+    @Before
+    public void setUp() {
+        // Mock the dependencies
+        userDataAccessObject = mock(GuestUserDataAccessInterface.class);
+        guestOutputBoundary = mock(GuestOutputBoundary.class);
+
+        // Initialize the GuestInteractor with the mocked dependencies
+        guestInteractor = new GuestInteractor(userDataAccessObject, guestOutputBoundary);
+    }
+
+}
