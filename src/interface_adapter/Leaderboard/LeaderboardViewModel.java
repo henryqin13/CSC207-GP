5da2b811159396518a8dac8adff0bf8c3e354d77
ronashkenazy <ronashkenazy04@gmail.com>
2023-12-03 14:05:23 -0500

    Finished first version of LeaderboardViewModel

--- src/interface_adapter/Leaderboard/LeaderboardViewModel.java
+++ src/interface_adapter/Leaderboard/LeaderboardViewModel.java
@@ -1,4 +1,22 @@
 package interface_adapter.Leaderboard;
+import interface_adapter.ViewModel;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
-public class LeaderboardViewModel {
+public class LeaderboardViewModel extends ViewModel {
+    public static final String TITLE_LABEL = "Leaderboard";
+    public static final String EXIT_BUTTON_LABEL = "Exit";
+    private LeaderboardState state = new LeaderboardState();
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    public LeaderboardViewModel(){super("Leaderboard");}
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+    public LeaderboardState getState(){return this.state;}
 }
